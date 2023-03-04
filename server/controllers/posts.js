211a4e0848148054598a17f1c1be42e08c1874f5
vishnuSekhar <vishnusekhar96@gmail.com>
2023-03-04 17:48:11 +0530

    fea:add-post-operations

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,70 @@
-import Post from "../models/Post";
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+/*Create Post */
+
+export const createPost = async (re, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+    const post = await Post.find(); //fetch all posts!
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+/*Read Posts */
+
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find(); //fetch all posts!
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId }); //fetch all posts!
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+/*Update */
+
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,7 +6,7 @@ import {
   getUserFriends,
   addRemoveFriend,
 } from "../controllers/users.js";
-import { verifyToken } from "../middleware/auth";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -16,3 +16,5 @@ router.get("/:id/friends", verifyToken, getUserFriends);
 
 /*UPDATE */
 router.patch("/:id/friendId", verifyToken, addRemoveFriend);
+
+export default router;
