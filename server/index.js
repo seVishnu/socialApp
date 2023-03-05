2248d92c5ecadf364e7e3da67cdee1ffe373bf91
vishnuSekhar <vishnusekhar96@gmail.com>
2023-03-05 16:04:34 +0530

    fea:add-client-init

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -57,5 +59,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log("Server is running on port:", PORT));
+    //insert once!!!
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log("CanT connect"));
