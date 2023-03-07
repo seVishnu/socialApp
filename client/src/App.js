8062fde57583464c86edd4a03b0c63cdb7221954
vishnuSekhar <vishnusekhar96@gmail.com>
2023-03-07 16:56:11 +0530

    fea:redux-toolkit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+const App = () => {
   return (
     <div className="app">
-      <header className="App-header"></header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
